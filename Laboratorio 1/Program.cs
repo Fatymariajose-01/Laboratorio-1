530d9fdecac8b54a1b3750a072eec5a448ee9dd0
Fatymariajose-01 <fmariajose2006@gmail.com>
2024-07-29 08:51:19 -0600

    Cree el método principal y el método para pedir los datos del producto el nombre y el precio.

--- Laboratorio 1/Program.cs
+++ Laboratorio 1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SolicitarProducto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolicitarDatosProducto();
+        }
+
+        static void SolicitarDatosProducto()
+        {
+            
+            Console.Write("Introduzaca el nombre del producto: ");
+            int nombreProducto = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Introduzca el precio del producto: ");
+            int precioProducto = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine($"Nombre del producto: {nombreProducto}");
+            Console.WriteLine($"Precio del producto: {precioProducto:}");
+        }
+    }
+}
