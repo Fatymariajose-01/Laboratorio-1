29c65668e73a3ff94e00a34bb6c722cfa8a6fe0d
Fatymariajose-01 <fmariajose2006@gmail.com>
2024-07-29 09:02:56 -0600

    Agregué un menú de productos.

--- Laboratorio 1/Program.cs
+++ Laboratorio 1/Program.cs
@@ -1,17 +1,22 @@
-﻿using System;
-
-namespace SolicitarProducto
+﻿namespace SolicitarProducto
 {
     class Program
     {
+   
         static void Main(string[] args)
         {
             SolicitarDatosProducto();
         }
-
+        static void MostrarMenu()
+        {
+            Console.WriteLine("...Productos...");
+            Console.WriteLine("1. Pelota...Q.80.000");
+            Console.WriteLine("2. Carrito...Q.20.00");
+            Console.WriteLine("3. Peluche...Q.15.00");
+        }
         static void SolicitarDatosProducto()
         {
-            
+
             Console.Write("Introduzaca el nombre del producto: ");
             int nombreProducto = Convert.ToInt32(Console.ReadLine());
 
@@ -20,7 +25,12 @@ namespace SolicitarProducto
 
 
             Console.WriteLine($"Nombre del producto: {nombreProducto}");
-            Console.WriteLine($"Precio del producto: {precioProducto:}");
+            Console.WriteLine($"Precio del producto: {precioProducto}");
         }
+        static void AcumularPrecio()
+        {
+            
+        }
+
     }
 }
